fc473c24604cbb811a036e0f2bbf8ea23ccd3e23
Igor Varyvoda <igor@varyvoda.com>
2023-10-17 14:17:29 +0200

    well it's working

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h2>Testing Sirv Nuxt Image provider</h2>
+    <NuxtImg provider="sirv" src="/image.png" width="500" format="png"/>
+    <NuxtImg provider="sirv" src="/image.jpeg" width="500" quality="95"/>
+    <NuxtImg provider="sirv" src="tesla2.jpg" width="1000" />
   </div>
 </template>
