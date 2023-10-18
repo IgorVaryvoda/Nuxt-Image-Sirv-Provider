fff0060dc1464dba2b83b4d20c8f75eab03a9572
Igor Varyvoda <igor@varyvoda.com>
2023-10-18 13:08:01 +0200

    Add more dynamic imaging options + fix some demos

--- app.vue
+++ app.vue
@@ -4,84 +4,58 @@
 
     <div class="mb-8">
       <h3 class="text-2xl mb-2">JPG to PNG conversion</h3>
-      <NuxtImg provider="sirv" src="/harris-large.jpg" width="500" format="png" placeholder class="shadow-lg"/>
+      <NuxtImg provider="sirv" src="/harris-large.jpg" width="500" format="png" placeholder class="shadow-lg" />
     </div>
     <div class="mb-8">
       <h3 class="text-2xl mb-2">Color modifiers</h3>
-      <NuxtImg provider="sirv" src="/harris-large.jpg" width="500" format="png" placeholder class="shadow-lg"
-         :modifiers='{
-            sharpen: 1,
-            saturation: 50,
-            contrast: 50,
-            brightness: 15
-         }'
-      />
+      <NuxtImg provider="sirv" src="/harris-large.jpg" width="500" format="png" placeholder class="shadow-lg" :modifiers='{
+        sharpen: 1,
+        saturation: 50,
+        contrast: 50,
+        brightness: 15
+      }' />
     </div>
     <div class="mb-8">
       <h3 class="text-2xl mb-2">Trim crop (removes whitespace)</h3>
-      <NuxtImg provider="sirv" src="/lacoste.jpg" width="500" quality="95" placeholder class="shadow-lg"
-      :modifiers='{
+      <NuxtImg provider="sirv" src="/lacoste.jpg" width="500" quality="95" placeholder class="shadow-lg" :modifiers='{
         crop: "trim",
-      }'
-      />
+      }' />
     </div>
 
     <div class="mb-8">
       <h3 class="text-2xl mb-2">500 width, cover fit</h3>
-      <NuxtImg provider="sirv" src="look-big.jpg" width="500" fit="cover" placeholder class="shadow-lg"
-      />
+      <NuxtImg provider="sirv" src="look-big.jpg" width="500" fit="cover" placeholder class="shadow-lg" />
     </div>
     <div class="mb-8">
       <h3 class="text-2xl mb-2">Face detection crop</h3>
-      <NuxtImg provider="sirv" src="look-big.jpg" height="500" placeholder class="shadow-lg"
-       :modifiers='{
-          crop: "face",
-       }'
-      />
+      <NuxtImg provider="sirv" src="look-big.jpg" height="500" placeholder class="shadow-lg" :modifiers='{
+        crop: "face",
+      }' />
     </div>
 
     <div class="mb-8">
       <h3 class="text-2xl mb-2">(fit=fill)</h3>
-      <NuxtImg provider="sirv" src="look-big.jpg" width="500" height="200" fit="fill" placeholder class="shadow-lg"/>
+      <NuxtImg provider="sirv" src="look-big.jpg" width="500" height="200" fit="fill" placeholder class="shadow-lg" />
     </div>
 
     <div class="mb-8">
       <h3 class="text-2xl mb-2">PDF file</h3>
-      <NuxtImg provider="sirv" src="QW.pdf" width="500"  placeholder class="shadow-lg"
-        :modifiers='{
-          page: 1,
-        }'
-      />
+      <NuxtImg provider="sirv" src="QW.pdf" width="500" placeholder class="shadow-lg" :modifiers='{
+        pdfPage: 1,
+      }' />
     </div>
 
     <div class="mb-8">
-      <h3 class="text-2xl mb-2">Two text overlays</h3>
-      <NuxtImg provider="sirv" src="look-big.jpg" height="500" placeholder class="shadow-lg"
-               :modifiers="{
-                text: [
-                {
-                  text:'Hello',
-                  position: 'north',
-                  size: '20%',
-                  color: 'ffffff'
-                  },
-                  {
-                text: 'World!',
-                  position: 'south',
-                  size: '34%',
-                  color: 'ffffff'
-                }
-               ]
-              }"
-      />
+      <h3 class="text-2xl mb-2">Text overlay</h3>
+      <NuxtImg provider="sirv" src="look-big.jpg" height="500" placeholder class="shadow-lg" :modifiers="{
+        text: 'Hello there', textAlign: 'center', textPositionGravity: 'south', textSize: 60, textFontFamily: 'Arial', textColor: 'white'
+      }" />
     </div>
     <div class="mb-8">
       <h3 class="text-2xl mb-2">Watermark overlay</h3>
-      <NuxtImg provider="sirv" src="t-shirt-man.jpg" height="500" placeholder class="shadow-lg"
-               :modifiers="{
-                 watermark: '/watermark-v1.png'
-              }"
-      />
+      <NuxtImg provider="sirv" src="t-shirt-man.jpg" height="500" placeholder class="shadow-lg" :modifiers="{
+        watermark: '/watermark-v1.png', watermarkPosition: 'center', watermarkWidth: '30%'
+      }" />
     </div>
 
   </div>
--- providers/sirv.ts
+++ providers/sirv.ts
@@ -1,83 +1,131 @@
 import { joinURL } from 'ufo'
-import {ProviderGetImage} from "@nuxt/image";
+import { ProviderGetImage } from "@nuxt/image";
 import { createOperationsGenerator } from '#image'
+const deleteHash = (value: string) => value.startsWith('#') ? value.replace('#', '') : value
 export const operationsGenerator = createOperationsGenerator({
-    keyMap: {
-        width: 'w',
-        height: 'h',
-        format: 'format',
-        quality: 'q',
-        profile: 'profile',
-        fit: 'scale.option',
-        crop: 'crop.type',
-        cropar: 'crop.aspectratio',
-        cw: 'cw',
-        ch: 'ch',
-        watermark: 'watermark',
-        sharpen: 'sharpen',
-        blur: 'blur',
-        grayscale: 'grayscale',
-        colorize: 'colorize',
-        colortone: 'colortone',
-        lightness: 'lightness',
-        hue: 'hue',
-        saturation: 'saturation',
-        highlights: 'highlights',
-        shadows: 'shadows',
-        brightness: 'brightness',
-        exposure: 'exposure',
-        contrast: 'contrast',
-        rotate: 'rotate',
-        pdfpage: 'page',
+  keyMap: {
+    width: 'w',
+    height: 'h',
+    s: 's',
+    quality: 'q',
+    fit: 'scale.option',
+    profile: 'profile',
+    format: 'format',
+    webpFallback: 'webp-fallback',
+    subsampling: 'subsampling',
+    gifCompression: 'gif.lossy',
+    crop: 'crop.type',
+    cropAr: 'crop.aspectratio',
+    cw: 'cw',
+    ch: 'ch',
+    cx: 'cx',
+    cy: 'cy',
+    cropPaddingX: 'crop.pad.x',
+    cropPaddingY: 'crop.pad.y',
+    canvasHeight: 'canvas.height',
+    canvasWidth: 'canvas.width',
+    canvasAr: 'canvas.aspectratio',
+    canvasPosition: 'canvas.position',
+    canvasBorderWidth: 'canvas.border.width',
+    canvasBorderHeight: 'canvas.border.height',
+    canvasBorderColor: 'canvas.border.color',
+    canvasBorderOpacity: 'canvas.border.opacity',
+    watermark: 'watermark',
+    watermarkPosition: 'watermark.position',
+    watermarkWidth: 'watermark.scale.width',
+    watermarkHeight: 'watermark.scale.height',
+    text: 'text',
+    textBase64: 'text.text64',
+    textSize: 'text.size',
+    textAlign: 'text.align',
+    textPosition: 'text.position',
+    textPositionX: 'text.position.x',
+    textPositionY: 'text.position.y',
+    textPositionGravity: 'text.position.gravity',
+    textFontSize: 'text.font.size',
+    textFontStyle: 'text.font.style',
+    textFontFamily: 'text.font.family',
+    textFontWeight: 'text.font.weight',
+    textColor: 'text.color',
+    textOpacity: 'text.opacity',
+    textOutlineWidth: 'text.outline.width',
+    textoutlineColor: 'text.outline.color',
+    textOutlineOpacity: 'text.outline.opacity',
+    textOutlineBlur: 'text.outline.blur',
+    textBackgroundColor: 'text.background.color',
+    textbackgroundOpacity: 'text.background.opacity',
+    sharpen: 'sharpen',
+    blur: 'blur',
+    grayscale: 'grayscale',
+    colorize: 'colorize',
+    colorizeColor: 'colorize.color',
+    colorizeOpacity: 'colorize.opacity',
+    colortone: 'colortone',
+    colortoneColor: 'colortone.color',
+    colortoneLevel: 'colortone.level',
+    colortoneMode: 'colortone.mode',
+    vignette: 'vigette.value',
+    vignetteColor: 'vigette.color',
+    lightness: 'lightness',
+    colorlevelBlack: 'colorlevel.black',
+    colorlevelWhite: 'colorlevel.white',
+    histogram: 'histogram',
+    hue: 'hue',
+    saturation: 'saturation',
+    highlights: 'highlights',
+    shadows: 'shadows',
+    brightness: 'brightness',
+    exposure: 'exposure',
+    contrast: 'contrast',
+    rotate: 'rotate',
+    flip: 'flip',
+    flop: 'flop',
+    opacity: 'opacity',
+    frameStyle: 'frame.style',
+    frameColor: 'frame.color',
+    frameWidth: 'frame.width',
+    frameRimColor: 'frame.rim.color',
+    frameRimWidth: 'frame.rim.width',
+    pdfPage: 'page',
+  },
+  valueMap: {
+    fit: {
+      fill: 'ignore',
+      inside: 'noup',
+      outside: 'min',
+      cover: 'fill',
+      contain: 'ignore',
+      fillMax: 'fillmax'
     },
-    valueMap: {
-        fit: {
-            fill: 'ignore',
-            inside: 'noup',
-            outside: 'min',
-            cover: 'fill',
-            contain: 'ignore',
-            fillMax: 'fillmax'
-        },
-        crop: {
-            face: 'face',
-            poi: 'poi',
-            trim: 'trim',
-        },
-        format: {
-            gif: 'gif',
-            jpeg: 'jpg',
-            jpg: 'jpg',
-            png: 'png',
-            webp: 'webp',
-            eps: 'eps',
-            psd: 'psd',
-            svg: 'svg',
-            tif: 'tif',
-            ai: 'ai',
-            heic: 'heic',
-            original: 'original',
-        }
+    crop: {
+      face: 'face',
+      poi: 'poi',
+      trim: 'trim',
     },
-    joinWith: '&',
-    formatter: (key: any, value: any) => `${key}=${value}`
+    format: {
+      jpeg: 'jpg',
+      original: 'original',
+    },
+    canvasBorderColor(value: string) {
+      return deleteHash(value)
+    },
+    frameColor(value: string) {
+      return deleteHash(value)
+    },
+    frameRimColor(value: string) {
+      return deleteHash(value)
+    },
+    colorizeColor(value: string) {
+      return deleteHash(value)
+    }
+  },
+  joinWith: '&',
+  formatter: (key: any, value: any) => `${key}=${value}`
 })
 
 export const getImage: ProviderGetImage = (src, { modifiers = {}, baseURL = '/' } = {}) => {
-    let additionalOperations = '';
-
-    if (modifiers.text) {
-        let textIndex = 0;
-        for (const textObj of modifiers.text) {
-            for (const [key, value] of Object.entries(textObj)) {
-                additionalOperations += `&text.${textIndex}.${key}=${value}`;
-            }
-            textIndex++;
-        }
-    }
-
-    const operations = operationsGenerator(modifiers);
-    return {
-        url: joinURL(baseURL, src + (operations ? ('?' + operations) : '')) + additionalOperations
-    };
+  const operations = operationsGenerator(modifiers);
+  return {
+    url: joinURL(baseURL, src + (operations ? ('?' + operations) : ''))
+  };
 }
