336b8d33d288e0e92fc6b7e6b8df1abe79f909b3
Igor Varyvoda <igor@varyvoda.com>
2023-11-01 11:41:29 +0100

    crossorigin anon

--- pages/format.vue
+++ pages/format.vue
@@ -1,4 +1,4 @@
 <template>
   <h2 class="text-2xl mb-2">Format conversion</h2>
-  <NuxtImg provider="sirv" src="/harris.jpg" width="500" format="webp" />
+  <NuxtImg provider="sirv" src="/harris.jpg" width="500" format="webp" crossorigin="anonymous" />
 </template>
